c8bb2c0c77fc323e2c1b59373d3732f881cbb669
Hanif <hanif.adedotun@gmail.com>
2026-04-19 15:09:28 -0400

    updated to v4

--- Formula/mirusync.rb
+++ Formula/mirusync.rb
@@ -5,8 +5,8 @@
 class Mirusync < Formula
     desc "Folder sync between two machines over SSH"
     homepage "https://github.com/hanif-adedotun/mirusync"
-    url "https://github.com/hanif-adedotun/mirusync/archive/refs/tags/v0.1.3.tar.gz"
-    sha256 "e7f6961c6b21c17f406cb61b820bc657415edb9a5e54b42fa548e95b7498ca04"
+    url "https://github.com/hanif-adedotun/mirusync/archive/refs/tags/v0.1.4.tar.gz"
+    sha256 "99400ae440b6ebfb881d8b4e3f592c3a0539e139557bad4e6b90f63744547419"
     version "0.1.3"
     license "MIT"
     head "https://github.com/hanif-adedotun/mirusync.git", branch: "main"
